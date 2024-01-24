fa6e3e004c35c0d4d39680c842e1ace469379b69
Asif Shikder <shikdernyc@gmail.com>
2024-01-24 00:12:31 -0500

    MINOR: Added request route for backend to get toolset

--- src/app/lambda.ts
+++ src/app/lambda.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from "../types";
 import CurrentProject from "../utils/current_project";
-import APIRequestHandler from "./request_handler";
+import EventHandler from "./request_handler";
 
 export const handler = async (event: any): Promise<any> => {
   const appConfig : ApplicationConfig = await CurrentProject.getApplicationConfig(false);
-  const app = new APIRequestHandler(appConfig);
-  const response = await app.eventHandler(event);
+  const app = new EventHandler(appConfig);
+  const response = await app.handle(event);
   return response;
 };
