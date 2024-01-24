d8607090c33c6db36aa27022b023662623838e4e
Asif Shikder <shikdernyc@gmail.com>
2024-01-24 00:38:46 -0500

    FIX: Formatting issues

--- src/types.ts
+++ src/types.ts
@@ -1,62 +1,60 @@
-import { OpenAI } from "openai";
-import { ZodSchema } from "zod";
-
+import { type ZodSchema } from 'zod'
 
 export interface IncludeStatement {
-  files: string[];
-  directories: string[];
+  files: string[]
+  directories: string[]
 }
 
 export interface PackageConfig {
   app: {
-    entryPath: string;
-    build: string[];
-  };
-  include: IncludeStatement;
+    entryPath: string
+    build: string[]
+  }
+  include: IncludeStatement
 }
 
 export enum ToolExecutionEnvironment {
-  Client = "client",
-  Local = "local",
+  Client = 'client',
+  Local = 'local',
 }
 
 interface IToolSchema<InputSchema = any, OutputSchema = any> {
-  name: string;
-  description: string;
-  input?: ZodSchema<InputSchema>;
-  output?: ZodSchema<OutputSchema>;
-  executionEnvironment: ToolExecutionEnvironment;
+  name: string
+  description: string
+  input?: ZodSchema<InputSchema>
+  output?: ZodSchema<OutputSchema>
+  executionEnvironment: ToolExecutionEnvironment
 }
 interface ClientToolSchema extends IToolSchema {
-  executionEnvironment: ToolExecutionEnvironment.Client;
+  executionEnvironment: ToolExecutionEnvironment.Client
 }
 
 interface LocalToolSchema extends IToolSchema {
-  executionEnvironment: ToolExecutionEnvironment.Local;
-  handler: (input: any) => Promise<any>;
+  executionEnvironment: ToolExecutionEnvironment.Local
+  handler: (input: any) => Promise<any>
 }
 
-export type ITool = ClientToolSchema | LocalToolSchema;
+export type ITool = ClientToolSchema | LocalToolSchema
 
 export interface Toolset {
-  name: string;
-  tools: ITool[];
+  name: string
+  tools: ITool[]
 }
 
 export interface PromptBuilder {
-  getSystemPrompt(): Promise<string>;
-  getPromptForQuery(query: string): Promise<string>;
+  getSystemPrompt: () => Promise<string>
+  getPromptForQuery: (query: string) => Promise<string>
 }
 
 export interface ProjectConfig {
-  orgId: number;
-  projectId: number;
-  apiKey: string;
+  orgId: number
+  projectId: number
+  apiKey: string
 }
 
 export interface ApplicationConfig {
-  project: ProjectConfig;
-  model: string;
-  promptBuilder: PromptBuilder;
-  toolset: Toolset;
+  project: ProjectConfig
+  model: string
+  promptBuilder: PromptBuilder
+  toolset: Toolset
 }
